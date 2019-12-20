337c6df0d0b1171bdede4e51033f149aae04ac9a
jiabao <1181488349@qq.com>
2019-12-20 21:10:44 +0800

    基本路由

--- www/laravel/routes/web.php
+++ www/laravel/routes/web.php
@@ -14,3 +14,34 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * 以下是测试路由功能用的
+ */
+
+//基本路由
+Route::get('/foo', function (){
+    return 'hello world!';
+});
+
+//重定向路由
+Route::redirect('/redirect', '/foo');//302重定向
+Route::redirect('/redirect301', '/foo', 301);//301重定向
+Route::permanentRedirect('permanentRedirect', '/foo');//301重定向
+
+//视图路由
+Route::view('/hello', 'hello');
+Route::view('/hello_laravel', 'hello_laravel', ['name'=>'Laravel']);
+
+//路由参数
+Route::get('hello/{name}', function ($name){//必填参数
+    return 'Hello, ' . $name;
+});
+
+Route::get('hello/{name}/good/{time}', function ($name, $time){//必填多参数
+    return 'Hello, ' . $name . '! good ' . $time . '!';
+});
+
+Route::get('/post/{id?}', function ($id = 1){//可选参数
+    return 'post_id = ' . $id;
+});
