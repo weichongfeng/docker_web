3eeb91c1d1295b0641c644d62bd9a32178846c83
fujiabao <1181488349@qq.com>
2020-01-08 18:41:45 +0800

    定义模型关联的表及主键

--- www/laravel/app/models/Category.php
+++ www/laravel/app/models/Category.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * 重新定义表明
+     */
+    protected $table = 'post';
+
+    /**
+     * 重新定义主键
+     */
+    protected $primaryKey = 'id';
 }
